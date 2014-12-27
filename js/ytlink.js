00ac3f039435e16d3917f29bc19a0a2452256f5a
gwcmk <gregwcmkim@gmail.com>
2014-12-26 20:16:31 -0500

    Added support for timestamps in URL

--- js/ytlink.js
+++ js/ytlink.js
@@ -9,9 +9,27 @@ if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navi
 			if($(".ytlink-container")[0]){
 				$(".ytlink-container").remove();
 			}
+			var idt = getIDAndTime(url);
+			var id = idt[1];
+			var time = 0;
 
-			var id = extractVideoID(url);
-			$(".href").after('<div class="ytlink-container"><iframe src="https://www.youtube.com/embed/' + id + '?rel=0&autoplay=1' + '" width="256" height="144" frameborder="0" class="video" allowfullscreen></iframe></div>');
+			if(typeof idt[2] != 'undefined'){
+				var hours = idt[2].match(/(\d{1,2})(?=h)/);
+				var minutes = idt[2].match(/(\d{1,2})(?=m)/);
+				var seconds = Number(idt[2].match(/(\d{1,2})(?=s)/)[0]);
+
+				//TODO: find more elegant solution for this problem
+				hours = (hours === null) ? [0] : Number(hours[0]);
+				minutes = (minutes === null) ? [0] : Number(minutes[0]);
+
+				time = hours*3600 + minutes*60 + seconds;
+			}
+			// console.log("hours = " + hours);
+			// console.log("mins = " + minutes);
+			// console.log("secs = " + Number(seconds));
+			// console.log("time = " + time);
+
+			$(".href").after('<div class="ytlink-container"><iframe src="https://www.youtube.com/embed/' + id + '?rel=0&autoplay=1&start=' + time +'" width="256" height="144" frameborder="0" class="video" allowfullscreen></iframe></div>');
 			return false;
 		}
 	});
@@ -21,8 +39,8 @@ if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navi
 
 	$(window).on("scroll", function(){
 		var st = $(window).scrollTop();
-		console.log("st = " + st);
-		console.log("ht = " + ht);
+		// console.log("st = " + st);
+		// console.log("ht = " + ht);
 		if(st < ht){
 			$(".ytlink-container").animate({bottom: -st}, 1);
 		}
@@ -31,15 +49,11 @@ if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navi
 
 }
 
-//taken from http://ctrlq.org/code/19797-regex-youtube-id
-function extractVideoID(url){
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-    var match = url.match(regExp);
-    if ( match && match[7].length == 11 ){
-        return match[7];
-    }else{
-        return false;
-    }
+//returns array with url, ID, and timestamp of video
+function getIDAndTime(url){
+	var regExp = /^(?:https?\:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v\=))([\w-]{10,12})(?:[\&\?\#].*?)*?(?:[\&\?\#]t=([\dhm]+s))?$/;
+	var match = url.match(regExp);
+	return match;
 }
 
 
